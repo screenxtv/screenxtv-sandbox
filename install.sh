52145d1553281215a130f5c33303b627b40eb55b
Yohei YASUKAWA <yasulab@gmail.com>
2012-12-03 16:21:53 +0900

    check command w/ which command and use '' for working directory

--- install.sh
+++ install.sh
@@ -5,22 +5,22 @@ IS_SCREENX_EXECUTABLE=0
 
 REPO_NAME="screenxtv-client" 
 PREVIOUS_DIR=$PWD
+TMP=/tmp
 
 # check if required commands installed.
 function check_command {
   local COMMAND_NAME="$1"
-  $COMMAND_NAME --version > /dev/null 2>&1
+  which $COMMAND_NAME > /dev/null 2>&1
   COMMAND_STATUS=$?
   
   # check if the given command is correctly invoked.  
-  # NOTE: `screen -v` returns 1 if successful, not 0.
-  if [ $COMMAND_STATUS -gt 1 ]; then
+  if [ $COMMAND_STATUS = 0 ]; then
+      echo "\t$1 is ...\tfound."
+      return 0
+  else
       echo "\t$1 is ...\tNOT found."
       IS_SCREENX_EXECUTABLE=1
       return 127
-  else
-      echo "\t$1 is ...\tfound."
-      return 0
   fi
 }
 
@@ -38,22 +38,22 @@ if [ $IS_SCREENX_EXECUTABLE -gt 0 ]; then
 fi
 
 
-if [ -s /tmp/$REPO_NAME ]; then
+if [ -s $TMP/$REPO_NAME ]; then
     echo "* already downloaded $REPO_NAME."
 else
     echo "* downloading ScreenX TV Client ..."
-    git clone https://github.com/tompng/$REPO_NAME.git --local /tmp/$REPO_NAME
+    git clone https://github.com/tompng/$REPO_NAME.git --local $TMP/$REPO_NAME
 fi
 echo ""
 
 # compile ScreenX TV Client
-g++ -o /tmp/$REPO_NAME/run /tmp/$REPO_NAME/main.cc -lpthread -lutil
+g++ -o $TMP/$REPO_NAME/run $TMP/$REPO_NAME/main.cc -lpthread -lutil
 if [ $? -gt 0 ]; then
     echo "* can't run ScreenX TV Client. Please check errors above."
     exit
 fi
 
 # run ScreenX TV Client
-cd /tmp/$REPO_NAME
+cd $TMP/$REPO_NAME
 echo "* going to start broadcasting ... \n"
-/tmp/$REPO_NAME/run
+$TMP/$REPO_NAME/run
